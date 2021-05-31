b79bf054acde995883c24dd92aaaed1a07fe533d
darinchakalov <darinchakalov@hotmail.com>
2021-05-31 17:46:18 +0300

    Functions - More Exercise

---
+++ Functions/05. Print DNA.js
@@ -0,0 +1,22 @@
+function printDNA(num) {
+    let dna = "ATCGTTAGGG".split('');
+    for (let i = 1; i <= num; i++) {
+        let [a, b] = dna.splice(0, 2);
+        if (i === 1 || i % 4 === 1) {
+            console.log(`**${a}${b}**`);
+        }
+        else if (i === 2 || i % 4 === 2) {
+            console.log(`*${a}--${b}*`);
+        }
+        else if (i === 3 || i % 4 === 3) {
+            console.log(`${a}----${b}`);
+        }
+        else if (i === 4 || i % 4 === 0) {
+            console.log(`*${a}--${b}*`);
+        }
+
+        dna.push(a);
+        dna.push(b);
+    }
+}
+printDNA(4)
