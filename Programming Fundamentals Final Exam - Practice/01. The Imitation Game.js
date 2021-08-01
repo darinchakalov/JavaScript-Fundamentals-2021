3b88129986868a53b84f2d678c7a88b96c0aa11b
darinchaklov <darinchakalov@hotmail.com>
2021-08-01 22:18:55 +0300

    final exam prep

---
+++ Programming Fundamentals Final Exam - Practice/01. The Imitation Game.js
@@ -0,0 +1,38 @@
+function solve(input) {
+    let message = input.shift();
+    let line = input.shift();
+    while (line !== "Decode") {
+        let [command, first, second] = line.split("|");
+        if (first.length > 0) {
+            switch (command) {
+                case "Move":
+                    let move = message.substring(0, Number(first));
+                    message = message.replace(move, "");
+                    message += move;
+                    break;
+                case "Insert":
+                    message = insert(message, Number(first), second)
+                    break;
+                case "ChangeAll":
+                    let rgx = new RegExp(first, 'g')
+                    message = message.replace(rgx, 'l')
+                    break;
+            }
+        }
+        line = input.shift();
+    }
+
+    console.log(`The decrypted message is: ${message}`);
+
+    function insert(str, index, value) {
+        return str.substr(0, index) + value + str.substr(index);
+    }
+}
+//solve(["zzHe", "ChangeAll|z|l", "Insert|2|o", "Move|3", "Decode"]);
+solve([
+    'owyouh',
+    'Move|2',
+    'Move|3',
+    'Insert|',
+    'Insert|9|?',
+    'Decode'])
