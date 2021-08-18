eccc5be12fdcdf573f0c57435f54f6ffbc76f42e
darinchaklov <darinchakalov@hotmail.com>
2021-08-18 06:34:02 +0300

    basic web project

---
+++ PhoneBook Web Project/controllers/phonebook-controller.js
@@ -0,0 +1,12 @@
+const phonebook = require('../phonebook');
+
+module.exports = {
+  index: (req, res) => {
+    res.render('index');
+    // TODO: load index page
+    
+  },
+  addPhonebookPost:(req, res) => {
+    // TODO: add a phonebook object to the array
+  }
+}
